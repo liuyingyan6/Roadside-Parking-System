c986a0414b15b47f7b38a9dc3a989b49990d69ea
phil <phil@gmail.com>
2023-09-06 20:27:40 +0800

    20230906/20:27前端

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from "@/views/Login.vue";
+import Login from '@/views/Login'
 import Home from '@/views/Home'
 import Role from '@/views/permission/Role'
 import Car from "@/views/permission/Car";
-import Order from "@/views/permission/Order.vue";
-import OrderDetails from "@/views/secondaryMenu/OrderDetails.vue";
+import ManagerAccount from "@/views/permission/ManagerAccount";
 
 Vue.use(VueRouter)
 
@@ -16,7 +15,7 @@ const routes = [
         component: Login
     },
     {
-        path: '/Login',
+        path: '/login',
         name: 'Login',
         component: Login
     },
@@ -32,21 +31,15 @@ const routes = [
                 component: Role
             },
             {
+                path: '/manager',
+                name: "ManagerAccount",
+                component: ManagerAccount
+            },
+            {
                 path: '/car',
                 name: "Car",
                 component: Car
-            },
-            {
-                path: '/order',
-                name: "Order",
-                component: Order,
-            },
-            {
-                path:'/orderDetails',
-                name: "OrderDetails",
-                component: OrderDetails
             }
-
         ]
     }
 ]
@@ -57,25 +50,25 @@ const router = new VueRouter({
     routes
 })
 
-// 前置守卫：运行在路由跳转之前
-router.beforeEach((to, form, next) => {
-    console.log("跳转路由之前，执行……………………");
-    let path = to.path;
-    if (path == '/' || path == '/Login') {
-        next();
-        return;//代码到此结束，不要继续向下执行
-    }
-    let id = localStorage.getItem("id");
-    if (!id) {
-        next("/");//没有登录，就定位到登录页面上去
-    } else {
-        next();//放行
-    }
-})
-//后置守卫：运行在路由跳转之后
-router.afterEach((to, form, next) => {
-    console.log("跳转路由之后，执行……………………");
-})
+// // 前置守卫：运行在路由跳转之前
+// router.beforeEach((to, form, next) => {
+//     console.log("跳转路由之前，执行……………………");
+//     let path = to.path;
+//     if (path == '/' || path == '/Login') {
+//         next();
+//         return;//代码到此结束，不要继续向下执行
+//     }
+//     let id = localStorage.getItem("id");
+//     if (!id) {
+//         next("/");//没有登录，就定位到登录页面上去
+//     } else {
+//         next();//放行
+//     }
+// })
+// //后置守卫：运行在路由跳转之后
+// router.afterEach((to, form, next) => {
+//     console.log("跳转路由之后，执行……………………");
+// })
 
 
 export default router
