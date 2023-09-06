b5ebf69c4f6a69eae86e9566f83a59074cc7077e
phil <phil@gmail.com>
2023-09-06 22:20:17 +0800

    20230906/22:20前端

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,31 @@ const routes = [
                 component: Car
             },
             {
+                path: '/user',
+                name: "User",
+                component: User
+            },
+            {
+                path: '/login-log',
+                name: "LoginLog",
+                component: LoginLog
+            },
+            {
+                path: '/operation-log',
+                name: "OperationLog",
+                component: OperationLog
+            },
+            {
+                path: '/magnetometer',
+                name: "Magnetometer",
+                component: Magnetometer
+            },
+            {
+                path: '/magnetometer-log',
+                name: "MagnetometerLog",
+                component: MagnetometerLog
+            },
+            {
                 path: '/order',
                 name: "Order",
                 component: Order,
@@ -63,25 +88,25 @@ const router = new VueRouter({
     routes
 })
 
-// 前置守卫：运行在路由跳转之前
-router.beforeEach((to, form, next) => {
-    console.log("跳转路由之前，执行……………………");
-    let path = to.path;
-    if (path == '/' || path == '/Login') {
-        next();
-        return;//代码到此结束，不要继续向下执行
-    }
-    let id = localStorage.getItem("id");
-    if (!id) {
-        next("/");//没有登录，就定位到登录页面上去
-    } else {
-        next();//放行
-    }
-})
-//后置守卫：运行在路由跳转之后
-router.afterEach((to, form, next) => {
-    console.log("跳转路由之后，执行……………………");
-})
+// // 前置守卫：运行在路由跳转之前
+// router.beforeEach((to, form, next) => {
+//     console.log("跳转路由之前，执行……………………");
+//     let path = to.path;
+//     if (path == '/' || path == '/Login') {
+//         next();
+//         return;//代码到此结束，不要继续向下执行
+//     }
+//     let id = localStorage.getItem("id");
+//     if (!id) {
+//         next("/");//没有登录，就定位到登录页面上去
+//     } else {
+//         next();//放行
+//     }
+// })
+// //后置守卫：运行在路由跳转之后
+// router.afterEach((to, form, next) => {
+//     console.log("跳转路由之后，执行……………………");
+// })
 
 
 export default router
