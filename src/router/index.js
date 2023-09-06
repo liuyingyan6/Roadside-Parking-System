4e4155c6045001ea785f0e4183b5084710977093
liuaa <892901548@qq.com>
2023-09-06 11:32:18 +0800

    新建loginlog.vue

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Login from '@/views/Login'
 import Home from '@/views/Home'
 import Role from '@/views/permission/Role'
 import Car from "@/views/permission/Car";
-import ManagerAccount from "@/views/permission/ManagerAccount";
+import LoginLog from "@/views/log/LoginLog.vue";
 
 Vue.use(VueRouter)
 
@@ -31,15 +31,15 @@ const routes = [
                 component: Role
             },
             {
-                path: '/manager',
-                name: "ManagerAccount",
-                component: ManagerAccount
-            },
-            {
                 path: '/car',
                 name: "Car",
                 component: Car
-            }
+            },
+            {
+                path: '/login-log',
+                name: "LoginLog",
+                component: LoginLog
+            },
         ]
     }
 ]
@@ -50,25 +50,25 @@ const router = new VueRouter({
     routes
 })
 
-// // 前置守卫：运行在路由跳转之前
-// router.beforeEach((to, form, next) => {
-//     console.log("跳转路由之前，执行……………………");
-//     let path = to.path;
-//     if (path == '/' || path == '/Login') {
-//         next();
-//         return;//代码到此结束，不要继续向下执行
-//     }
-//     let id = localStorage.getItem("id");
-//     if (!id) {
-//         next("/");//没有登录，就定位到登录页面上去
-//     } else {
-//         next();//放行
-//     }
-// })
-// //后置守卫：运行在路由跳转之后
-// router.afterEach((to, form, next) => {
-//     console.log("跳转路由之后，执行……………………");
-// })
+// 前置守卫：运行在路由跳转之前
+router.beforeEach((to, form, next) => {
+    console.log("跳转路由之前，执行……………………");
+    let path = to.path;
+    if (path == '/' || path == '/Login') {
+        next();
+        return;//代码到此结束，不要继续向下执行
+    }
+    let id = localStorage.getItem("id");
+    if (!id) {
+        next("/");//没有登录，就定位到登录页面上去
+    } else {
+        next();//放行
+    }
+})
+//后置守卫：运行在路由跳转之后
+router.afterEach((to, form, next) => {
+    console.log("跳转路由之后，执行……………………");
+})
 
 
 export default router
